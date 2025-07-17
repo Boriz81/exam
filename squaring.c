326ca0cc58a827866737884479556257157e8df8
Boriz81 <120014741+Boriz81@users.noreply.github.com>
2025-07-17 18:43:48 +0900

    Add files via upload

--- squaring.c
+++ squaring.c
@@ -1,22 +1,6 @@
 #include <stdio.h>
 #define NMAX 10
 
-int input(int *a, int *n);
-void output(int *a, int n);
-void squaring(int *a, int n);
-
-int main() {
-    int n = 0, data[NMAX];
-
-    input(data, &n);
-
-    squaring(data, n);
-
-    output(data, n);
-
-    return 0;
-}
-
 int input(int *a, int *n) {
     if (scanf("%d", n) != 1 || *n <= 0 || *n > NMAX) {
         printf("n/a");
@@ -25,19 +9,27 @@ int input(int *a, int *n) {
     for (int *p = a; p - a < *n; p++) {
         if (scanf("%d", p) != 1) {
             return 0;
-        };
+        }
     }
     return 1;
-}
-
-void output(int *a, int n) {
-    for (int i = 0; i < n; i++) {
-        printf("%d ", a[i]);
-    }
 }
 
 void squaring(int *a, int n) {
     for (int *p = a; p - a < n; p++) {
         *p = (*p) * (*p);
     }
+}
+
+void output (int *a, int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+}
+
+int main () {
+    int n = 0, data[NMAX];
+    input (data, &n);
+    squaring(data, n);
+    output(data, n);
+    return 0;
 }
