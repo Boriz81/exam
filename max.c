cf96e68584c8665ff665268acf79643a419e9de6
Boriz81 <120014741+Boriz81@users.noreply.github.com>
2025-07-30 21:31:54 +0900

    Update max.c

--- max.c
+++ max.c
@@ -1,30 +1,24 @@
 #include <stdio.h>
-
+ 
 int main() {
+ 
     int x, max;
-    int first_number = 1;  // Флаг: 1 = число ещё не введено, 0 = введено
-    
-    while (scanf("%d", &x) == 1) {  // Читаем числа, пока успешно
+    int first = 1;  // Флаг: 1 = число ещё не введено, 0 = введено
+ 
+    while (scanf("%d", &x)) {  // Читаем числа, пока успешно
         if (x == -1) {
             break;
         }
-        
-        // Если это первое число, инициализируем max
-        if (first_number) {
+
+        // Проверям, что после числа идет пробел или перенос строки
+        if (first) {
             max = x;
-            first_number = 0;
-        } 
-        // Иначе сравниваем с текущим максимумом
-        else if (x > max) {
-            max = x;
+            first = 0;
+        } else if (x > max) {
+                   max = x;
         }
     }
-    
-    // Если чисел не было (или только -1), завершаем с ошибкой
-    if (first_number) {
-        return 1;
-    }
-    
+ 
     printf("%d", max);
     return 0;
 }
