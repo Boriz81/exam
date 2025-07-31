b0091f1651777e8aa4151cef7c84daa066b4c8d3
boriz nekifirov <boriz@borizs-MacBook-Air.local>
2025-07-31 21:30:29 +0900

    ball2_var

--- platinum_ball.c
+++ platinum_ball.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <math.h>
 
-void calc_master(int r, double *m) {
-    *m = (4 / 3.0) * acos(-1.0) * pow(r, 3) * 21500;
+double calc_mass(double r) {
+    double v = 4.0 / 3.0 * (acos(-1.0)) * r * r * r;
+    return v * 21500;
 }
 
 int main() {
-    int r;
-    double m;
+    
+    double r;
 
-    if (scanf("%d", &r) != 1 || (getchar() != '\n' && getchar() != EOF)) {
+    if (scanf("%lf", &r) != 1 || (getchar() != '\n' && getchar() != EOF)) {
 	printf("n/a");
 	return 1;
     }
-    calc_master(r, &m);
-    printf("%.0lf", m);
+
+    printf("%.0f", round(calc_mass(r)));
     return 0;
 }
